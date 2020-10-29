b187b7a47e9e194bf21bc2311f2b23aa60a3a1d8
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-10-29 09:04:07 -0400

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,3 +4,22 @@ const assertEqual = function(actual, expected) {
   }
   return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
 };
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  let areEqual = true;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      areEqual = false;
+    }
+  }
+  return areEqual;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
