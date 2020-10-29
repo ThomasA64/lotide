613f94ac6d2c09077e1fa5eafb22a44253542638
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-10-29 08:54:09 -0400

    Update head.js

--- head.js
+++ head.js
@@ -1,14 +1,20 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
   }
+  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
 };
 
-function head([],) {
-  return head[0]
-}
+var head =  function(array, n) {
+  if (array == null) 
+  return void 0;
+if (n == null) 
+  return array[0];
+if (n < 0)
+  return [];
+return array.slice(0, n);
+};
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
