f86ecfcaea50f0db788af8650cc202c622794ff8
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-11-25 13:13:29 -0500

    Implemented Countonly function properly

--- countOnly.js
+++ countOnly.js
@@ -1,6 +1,50 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-  return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   }
-  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 };
+
+//countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.
+// Only keys which have a truthy value should be counted in the resulting object. All other strings (either set to false or not included at all in the object) should not be counted. That said, if a particular string is meant to be counted but does not exist in the input array (like "f" in the example above), it also does not have to be included in the final count.
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item] === true) {
+      console.log(item);
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+console.log(assertEqual(result1["Jason"], 1));
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Fang"], 2));
+console.log(assertEqual(result1["Agouhanna"], undefined));
