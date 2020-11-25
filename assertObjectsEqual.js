e9cca7b83d4cfec198320442dc6bc1310e9f8c30
Thomas Arthur <67028802+ThomasA64@users.noreply.github.com>
2020-11-25 17:42:11 -0500

    Implemented the AssertObjectEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,6 +1,22 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-  return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+const eqObjects = function (object1, object2) {
+  let Obj1Key1 = Object.keys(object1);
+  let Obj2Key2 = Object.keys(object2);
+
+  if (Obj1Key1.length !== Obj2Key2.length) {
+    return false;
   }
-  return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+  for (const Objkey of Obj1Key1) {
+    if (object1[Objkey] !== object2[Objkey]) {
+      return false;
+    }
+    return true;
+  }
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  if (eqObjects(Object1, Object2)) {
+    return `✅✅✅ Assertion Passed: ${Object1} === ${Object2} in data types and length\n`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${Object1} !== ${Object2} in data types or length\n`;
+  }
 };
